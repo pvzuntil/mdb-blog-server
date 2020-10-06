bc009257311ea4187fbfa21adfae427cca85ecec
untillness <pvauntil@gmail.com>
2020-10-06 13:50:58 +0700

    add: gitignore

--- route/authRoute.js
+++ route/authRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const makeRes = require('../lib/response');
 const UserModel = require('../model/User')
 const validation = require('../model/validation')
+const bycrypt = require('bcryptjs')
 
 router.post('/register', async (req, res) => {
     let data = req.body
@@ -9,10 +10,16 @@ router.post('/register', async (req, res) => {
     let { error } = validation.registerValidation(data)
     if (error) return res.status(400).send(makeRes(0, 'Validasi gagal !', error.details[0]))
 
+    const checkEmail = await UserModel.findOne({email: data.email})
+    if(checkEmail) return res.status(400).send(makeRes(0, 'Email sudah ada pak !'))
+
+    let passSalt = await bycrypt.genSalt(10)
+    let passHash = await bycrypt.hash(data.password, passSalt)
+
     const user = UserModel({
         name: data.name,
         email: data.email,
-        password: data.password
+        password: passHash
     })
 
     try {
